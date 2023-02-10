e3991f13e99b963eb537ff2c9e39b9b63f3a9ad5
Sachin Acharya <076bct060.sachin@pcampus.edu.np>
2023-02-10 20:44:37 +0545

    Vertical and horizontal alignment

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.littlelemon
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -21,7 +18,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.widget.ImageViewCompat
+
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,17 +39,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-@Composable
-fun GUI(name:String,size:Int)
+fun GUI()
 {
     LittleLemonTheme {
-        Column {
+        Column(Modifier.fillMaxSize()
+        , verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
             Title()
-            body()
+            Body()
         }
     }
 }
@@ -63,7 +59,7 @@ fun Title()
     Text(text =  stringResource(R.string.address), fontSize = 16.sp)
 }
 @Composable
-fun body()
+fun Body()
 {
     LittleLemonTheme{
         Row()
@@ -82,6 +78,6 @@ fun body()
 @Composable
 fun DefaultPreview() {
     LittleLemonTheme {
-        GUI(name = "MyApp", size =12 )
+        GUI()
     }
 }
