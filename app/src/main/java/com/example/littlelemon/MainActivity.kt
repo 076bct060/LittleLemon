746f8ee4981a33b43f20995edf75cef339f4b1bb
Sachin Acharya <sachinacharya44@gmail.com>
2023-03-02 21:38:29 +0545

    State Management

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,31 +3,37 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.modifier.modifierLocalConsumer
+
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.unit.sp
 
-import com.example.littlelemon.ui.theme.LittleLemonTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-           HomeScreen()
+//           HomeScreen()
+
+                AddItem()
+
+
 
 
             }
@@ -35,12 +41,60 @@ class MainActivity : ComponentActivity() {
     }
 
 
+//@Composable
+//fun HomeScreen(){
+//    Column() {
+//        uppperPannel()
+//        LowerPanel()
+//
+//    }
+//}
 @Composable
-fun HomeScreen(){
-    Column() {
-        uppperPannel()
-        LowerPanel()
+private fun AddItem()
+{
+    var count by rememberSaveable() {
+        mutableStateOf(0)
+    }
+    Column( modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally
+    , verticalArrangement = Arrangement.Center) {
+        Card(modifier = Modifier.fillMaxWidth(), elevation = 30.dp) {
+            Column(horizontalAlignment = Alignment.Start, modifier = Modifier.padding(20.dp)) {
+                Text("Greek Salad",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Row( modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+                    IconButton(onClick = { count-- }) {
+                        Icon(imageVector = Icons.Default.Remove, contentDescription ="Remove" )
+                    }
+                    Text(text = count.toString(), fontWeight = FontWeight.Bold, fontSize = 32.sp)
+                    IconButton(onClick = { count++ }) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription ="Add count" )
+                    }
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+                        Text("Add")
+
+                    }
+
+                }
+            }
+
+        }
+
 
     }
+
 }
 
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun AddItemPreview(){
+    AddItem()
+}
